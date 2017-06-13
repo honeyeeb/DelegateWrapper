d453d73cac2797917116debdc5bbd53f3b27c367
honeyeeb <fregrose7034@163.com>
2017-06-13 15:17:02 +0800

    Initial Commit

---
+++ DelegateWrapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DelegateWrapper
+//
+//  Created by Frederic on 2017/6/13.
+//  Copyright © 2017年 honeyeeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DelegateWrapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelegateWrapper
+//
+//  Created by Frederic on 2017/6/13.
+//  Copyright © 2017年 honeyeeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
